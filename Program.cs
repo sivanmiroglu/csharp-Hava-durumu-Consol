0d66daab4fba45acd892d0b1fc33067381c2d569
SivanMiroglu <96174950+sivanmiroglu@users.noreply.github.com>
2022-10-14 22:36:01 +0300

    Csharp Konsolda Hava durumunu gösteren uygulama

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
+
+namespace weather_fore
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string api = "0a033d04e0bfb4e50f80f6fed5b278de";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument weather = XDocument.Load(connection);
+            var temp = weather.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            var weatherstate = weather.Descendants("weather").ElementAt(0).Attribute("value").Value;
+            Console.Write("İstanbul için sıcaklık : " + temp + " --- Hava Durumu :" + weatherstate);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Csharp konsol da hava durumunu gösteren uygulama
+
+https://home.openweathermap.org/ adresinden api alındı. 
+
+Api konum bilgileri eklenerek bağlantı  XML türüne çevrildi 
+
+Csharp konsol tarafında xml türüne uygun kodlar yazılarak. Ekrana hava durumunu bildirimi yansıtıldı.
